7851ec05affbe10c67cac434b48043352fcefb54
rooal01 <alan.rooney97@gmail.com>
2020-12-15 15:36:35 +0000

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,23 +1,21 @@
-class minecraft (
- $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar'
- $install_dir = '/opt/minecraft'
- ){
-  file {$install_dir:
+ass minecraft (
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.1/minecraft_server.1.12.1.jar',
+  $install_dir = '/opt/minecraft'
+){
+  file { $install_dir:
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
-    source      => $url,
-    before => Service['minecraft'],
+    source => $url,
   }
+
   package {'java':
     ensure => present,
   }
   file {"${install_dir}/eula.txt":
     ensure => file,
-    content => epp('minecraft/minecraft.service',{
-    install_dir => $install_dir,
-    })
+    content => 'eula=true'
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
@@ -26,7 +24,5 @@ class minecraft (
   service { 'minecraft':
     ensure => running,
     enable => true,
-    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
-
