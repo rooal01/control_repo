1b4eac61bd5f2aa242ca1a5856e72e43e9cb9f3a
rooal01 <alan.rooney97@gmail.com>
2020-12-15 12:23:28 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,12 @@
 node default {
-  file {'/root/README':
-   ensure => file,
-   content => $fqdn,
-   owner => 'root',
-   }
    }
 node 'master.puppet.vm' {
 include role::master_server
+  file {'/root/README':
+   ensure => file,
+   content => "Welcome to ${fqdn}\n",
+   owner => 'root',
+   }
 }
 
 node /^web/ {
